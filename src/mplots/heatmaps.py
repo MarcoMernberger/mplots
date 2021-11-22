31583f7e1e1b9919c0002a2b78f3866547d3b5ac
Marco Mernberger <marco.mernberger@gmail.com>
2021-11-22 16:46:04 +0100

    added heatmap import

--- src/mplots/heatmaps.py
+++ src/mplots/heatmaps.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from pandas import DataFrame
 from matplotlib.figure import Figure
 from matplotlib.colors import Colormap
-from typing import Union
+from typing import Union, Tuple
 
 
 __author__ = "Marco Mernberger"
@@ -21,9 +21,35 @@ def heatmap(
     cmap: Union[str, Colormap] = "seismic",
     center: bool = True,
     shrink: float = 0.5,
-    figsize=(10, 10),
+    figsize: Tuple[int, int] = (10, 10),
     **kwargs
 ) -> Figure:
+    """
+    A simple heatmap.
+
+    Returns a simple heatmap that plots values of a given DataFrame. Figure size,
+    colormap, legend shrinkage parameter can be supplied. additional keyword
+    arguments are supplied to matplotlib's imshow function. X and Y labels
+    correspond to DataFrame column and row index.
+
+    Parameters
+    ----------
+    df_plot : DataFrame
+        DataFrame with values to be plotted.
+    cmap : Union[str, Colormap], optional
+        The ColorMap to use, by default "seismic".
+    center : bool, optional
+        Center the colorbar on zero, by default True.
+    shrink : float, optional
+        Shrinkage for colorbar, by default 0.5.
+    figsize : Tuple[int, int], optional
+        Figure size in inches, by default (10, 10).
+
+    Returns
+    -------
+    Figure
+        The plotted figure.
+    """
     f = plt.figure(figsize=figsize)
     if center:
         vmax = df_plot.abs().max().max()
